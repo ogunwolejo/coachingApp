31ad126d6b7d94cb2ddf73e5f91ecbc1d7379515
ogunwole joshua <71991059+ogunwolejo@users.noreply.github.com>
2024-02-11 20:43:12 +0100

    Update AppRoutes.tsx

--- src/app/routing/AppRoutes.tsx
+++ src/app/routing/AppRoutes.tsx
@@ -14,33 +14,33 @@ import {BarLoader, CircleLoader} from 'react-spinners'
 
 const AppRoutes: FC = () => {
   
-  const dispatch = useDispatch<ThunkDispatch<any,any, any>>()
+  // const dispatch = useDispatch<ThunkDispatch<any,any, any>>()
   
-  const {isAuth, loading} = useSelector((store:any) => ({
-    isAuth:store.auth.isAuth,
-    loading:store.auth.loading
-  }))
+  // const {isAuth, loading} = useSelector((store:any) => ({
+  //   isAuth:store.auth.isAuth,
+  //   loading:store.auth.loading
+  // }))
 
   
-   // Use the useLayoutEffect hook to render the PrivateRoute component before the page layout is painted
-   const fetch = async(token:string) => {
-      try {
-        await dispatch(AuthThunk.authByTokenThunk({token}))
-      } catch (error) {
-        console.error(error)
-      }
-   } 
+  //  // Use the useLayoutEffect hook to render the PrivateRoute component before the page layout is painted
+  //  const fetch = async(token:string) => {
+  //     try {
+  //       await dispatch(AuthThunk.authByTokenThunk({token}))
+  //     } catch (error) {
+  //       console.error(error)
+  //     }
+  //  } 
    
-   useLayoutEffect(() => {
-      if(isAuth) {
-        return 
-      }
-      // when it is undefined
-      const userExist:string | null = localStorage.getItem('token')
-      if(userExist) {
-        fetch(userExist)
-      }
-   }, [isAuth]);
+  //  useLayoutEffect(() => {
+  //     if(isAuth) {
+  //       return 
+  //     }
+  //     // when it is undefined
+  //     const userExist:string | null = localStorage.getItem('token')
+  //     if(userExist) {
+  //       fetch(userExist)
+  //     }
+  //  }, [isAuth]);
 
   
 
@@ -50,17 +50,10 @@ const AppRoutes: FC = () => {
         <Route element={<App />}>
           <Route path='error/*' element={<ErrorsPage />} />
           <Route path='logout' element={<Logout />} />
-          {isAuth ? (
-            <>
-              <Route path='/*' element={<PrivateRoutes />} />
-              {<Route index element={<Navigate to='/dashboard' />} />}
-            </>
-          ) : (
-            <>
-              <Route path='auth/*' element={<AuthPage />} />
-              <Route path='*' element={<Navigate to='/auth' />} />
-            </>
-          )}
+          <Route path='/*' element={<PrivateRoutes />} />
+          <Route index element={<Navigate to='/dashboard' />} />
+          <Route path='auth/*' element={<AuthPage />} />
+          <Route path='*' element={<Navigate to='/auth' />} />
         </Route>
       </Routes>
     </BrowserRouter>
